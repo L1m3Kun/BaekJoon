0b84ac7221c6611992cf331cc3b81028f71bdbad
L1m3Kun <sungku2757@gmail.com>
2023-01-16 23:26:08 +0900

    phase3 clear

--- phase_3/Cycle_plus.py
+++ phase_3/Cycle_plus.py
@@ -1,15 +1,14 @@
-num = input()
-count=1
-tmp =""
+num = int(input())
 tmp = num
+count =0
 while True:
-    if int(tmp)<10:
-        tmp = tmp+tmp
-       # print(tmp)
+    if tmp<10:
+        tmp = int(str(tmp)+str(tmp))
     else:
-        tmp = list(tmp)[1]+str(int(list(tmp)[0])+int(list(tmp)[1]))
-        #print(tmp)
-    count+=1
-    if num == tmp:
+        one = tmp%10
+        ten = tmp//10
+        tmp = int(str(one)+str((one+ten)%10))
+    count +=1
+    if tmp == num:
         break
 print(count)
