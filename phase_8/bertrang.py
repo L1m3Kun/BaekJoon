77626cb863ff47277b28dc82230e630993cae7d6
L1m3Kun <sungku2757@gmail.com>
2023-01-20 12:59:46 +0900

    중간 저장

--- phase_8/bertrang.py
+++ phase_8/bertrang.py
@@ -14,22 +14,14 @@
 #             count += 1
 #     return count
 def bertrang_theory(n):
-    count = 0
-    
-    for num in range(n+1, 2*n+1):                           # n보다 크고  2n보다 작거나 같은
-        if num in prime_lst:                                # 소수 리스트에 있으면 count +1
-            count += 1
-        else:                                               # 리스트에 없으면 소수 확인 
-            for idx in range(2, int(num**0.5) +1):          # 제곱근까지만 확인
-                if num % idx == 0:                          
-                    break
-            else:                                           # 소수이면 count + 1
-                prime_lst.append(num)                       # 소수 저장
-                count += 1
-    return count
+    prime_lst = [0,0] + [1] * (2 * n - 1)    
+    for num in range(2, 2*n+1):                           # 에라토스테네스의 채
+        if prime_lst[num]:
+            for idx in range(num * 2, 2 * n + 1, num):      # 2부터 2n까지의 배수들을 리스트에서 제거
+                prime_lst[idx] = 0                      
+    return sum(prime_lst[n+1:])
 while True:
     n = int(input())
-    prime_lst = []
     if n == 0:
         break
     print(bertrang_theory(n))
--- phase_8/find_prime.py
+++ phase_8/find_prime.py
@@ -8,9 +8,9 @@ prime = []
 for number in num:
     if number > 1:
         count =0
-        for less in range(2, number):
+        for less in range(2, number):   # 배수가 있는가 
             if number % less == 0:
                 count = 1
         if count == 0:
-            prime.append(number)
+            prime.append(number)        # 소수 모으기
 print(len(prime))
