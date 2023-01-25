3dfc964b257f0decb3225c5b4297ef1513eb8894
L1m3Kun <sungku2757@gmail.com>
2023-01-25 17:49:15 +0900

    18:00

--- phase_11/10872_factorial.py
+++ phase_11/10872_factorial.py
@@ -1,7 +1,7 @@
 def factorial(n):
-    if n == 1 or n == 0:
+    if n == 1 or n == 0:            # 재귀 탈출
         return 1
-    return factorial(n-1) * n
+    return factorial(n-1) * n       # 재귀 호출 n을 1씩 줄이면서
 
 N = int(input())
 print(factorial(N))
