2273e8f93ad3aed3dae727c6d169dc8683f85e15
L1m3Kun <sungku2757@gmail.com>
2023-01-25 15:50:19 +0900

    phase_9 clear

--- phase_8/gordbah.py
+++ phase_8/gordbah.py
@@ -1,5 +1,29 @@
 T = int(input())
+def prime(n):                       # is Prime?
+    if n == 2:
+        return True
+    else:
+        for idx in range(2,int(n **(1/2)) + 1):
+            if n % idx ==0:
+                return False
+        else:
+            return True
+
 for test_case in range(T):
+    n = int(input())
+    less_half = more_half = n // 2
+    while True:
+        if prime(less_half) and prime(more_half):   # 둘다 소수이면
+            print(less_half, more_half)             # 출력
+            break
+        elif less_half == 1:
+            break
+        else:
+            less_half -= 1
+            more_half += 1
+    
+
+''' 시간단축
     n = int(input())
     prime = [False, False] + [True] * (n - 1)
     for idx in range(2,n):
@@ -16,3 +40,4 @@ for test_case in range(T):
         print(num, n-num)
     else:
         print(n - num, num)
+'''
