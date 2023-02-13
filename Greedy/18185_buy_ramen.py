d3f21655f432c54fb58606d52d6bab98e80b7965
L1m3Kun <sungku2757@gmail.com>
2023-02-13 17:57:16 +0900

    2013 18:00

--- Greedy/18185_buy_ramen.py
+++ Greedy/18185_buy_ramen.py
@@ -1,17 +1,23 @@
 import sys
-
 N = int(sys.stdin.readline().strip())
 buy_list = list(map(int, sys.stdin.readline().strip().split()))
 i = 0
 cost = 0
 while i < N:
     if buy_list[i]:
-        if i < N-2 and buy_list[i]>0 and buy_list[i+1]>0 and buy_list[i+2]>0 and buy_list[i+1] <= buy_list[i+2]:
-            mini = min(buy_list[i],buy_list[i+1], buy_list[i+2])
+        if i < N-2 and buy_list[i]>0 and buy_list[i+1]>0 and buy_list[i+2]>0 and buy_list[i+1]<=buy_list[i+2]:
+            mini = min(buy_list[i], buy_list[i + 1], buy_list[i + 2])
             cost += 7 * mini
             buy_list[i] -= mini
-            buy_list[i+1] -= mini
-            buy_list[i+2] -= mini
+            buy_list[i + 1] -= mini
+            buy_list[i + 2] -= mini
+            if buy_list[i]:
+                continue
+        elif i < N-2 and buy_list[i]>0 and buy_list[i+1]>0 and buy_list[i+2]>0:
+            # mini = min(buy_list[i], buy_list[i + 1])
+            cost += 5
+            buy_list[i] -= 1
+            buy_list[i + 1] -=1
             if buy_list[i]:
                 continue
         if i < N-1 and buy_list[i]>0 and buy_list[i+1]>0:
@@ -21,10 +27,9 @@ while i < N:
             buy_list[i + 1] -= mini
             if buy_list[i]:
                 continue
-            i += 1
         else:
             cost += 3 *buy_list[i]
-            i += 1
+        i += 1
     else:
         i += 1
 print(cost)
