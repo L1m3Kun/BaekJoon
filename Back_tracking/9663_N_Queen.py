ea5b85d0922c11a407562b16a826b1d822fa200b
L1m3Kun <sungku2757@gmail.com>
2023-02-16 17:38:53 +0900

    0216 18:00

--- Back_tracking/9663_N_Queen.py
+++ Back_tracking/9663_N_Queen.py
@@ -1,6 +1,28 @@
-import sys
+def is_promising(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return False
 
-N = int(sys.stdin.readline().strip())
-arr = [[0] * N for _ in range(N)]
-while True:
-    queen = (0,0)
+    return True
+
+
+def n_queens(x):
+    global ans
+    if x == n:
+        ans += 1
+        return
+
+    else:
+        for i in range(n):
+            # [x, i]에 퀸을 놓겠다.
+            row[x] = i
+            if is_promising(x):
+                n_queens(x + 1)
+
+n = int(input())
+
+ans = 0
+row = [0] * n       # 체시판의 열
+
+n_queens(0)
+print(ans)
