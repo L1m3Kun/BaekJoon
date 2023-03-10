d41824177817bd6f58a93023783504d65d9166dd
L1m3Kun <sungku2757@gmail.com>
2023-03-10 17:56:31 +0900

    0310 18:00

---
+++ Tree/1167_dia_tree.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+V = int(input())
+tree = [[] for _ in range(V+1)]
+for _ in range(V):
+    arr = list(map(int, input().strip().split()))
+    
+    for i in range(1, len(arr)-1, 2):
+        if (arr[0], arr[i+1]) not in tree[arr[i]]:
+            tree[arr[0]].append((arr[i], arr[i+1])) 
+print(tree)
+max_val = max_value = 0
+for i in range(1, (V+1)//2):
+    stack = deque([i])
+    visited = [0] * (V+1)
+    visited[i] = 1
+    while stack:
+        j = stack.popleft()
+        for k, dis in tree[j]:
+            if max_val < dis:
+                spot = k
+                max_val = dis
+        if not visited[spot]:
+            visited[spot] = visited[j] + max_val
+            stack.append(spot)
+    max_value = max(max_value, *visited)
+            # if not visited[k]:
+            #     visited[k] += visited[j] + dis
+            #     stack.append(k)
+                # if max_val < visited[k]:
+                #     max_val=visited[k]
+print(max_value-1)       
--- Tree/11725_tree_parents.py
+++ Tree/11725_tree_parents.py
@@ -1,20 +1,23 @@
 import sys
+from collections import deque
 
 N = int(sys.stdin.readline().strip())
-arr = [[0] * N for _ in range(N)]
+arr = [[] for _ in range(N+1)]
 for _ in range(N-1):
     x, y = map(int, sys.stdin.readline().strip().split())
-    arr[x-1][y-1] = 1
-    arr[y-1][x-1] = 1
-par = [[] for _ in range(N)]
-for i in range(N):
-    for j in range(N):
-        if arr[i][j] == 1:
-            par[i].append(j)
-arr = []
-while True:
-    pass
-print(par)
+    arr[x].append(y)
+    arr[y].append(x)
+
+queue = deque([1])
+visited = [0] * (N+1)
+visited[1] = 1
+while queue:
+    i = queue.popleft()
+    for j in arr[i]:
+        if not visited[j]:
+            visited[j] = i
+            queue.append(j)
+print(*visited[2:], sep='\n')
 
 
 
