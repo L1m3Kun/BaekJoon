b4c6436b95df2003b32506773b8efe7875287520
L1m3Kun <sungku2757@gmail.com>
2023-03-16 17:55:26 +0900

    0316 18:00

--- Dinamic_Programming/9251_LCS.py
+++ Dinamic_Programming/9251_LCS.py
@@ -1,22 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-s1 = '0'+input().strip()
-s2 = '0'+input().strip()
+s1 = input().strip()
+s2 = input().strip()
 
-dp = [0] * len(s1)
-cnt = 1
-for i in range(1,len(s2)):
-    if s2[i] == s2[i-1]:
-        cnt += 1
-    else:
-        cnt = 1
-    tmp = cnt
+dp = [[0] * (len(s2)+1) for _ in range(len(s1)+1)]
 
+for i in range(1, len(s2)):
     for j in range(1, len(s1)):
         if s2[i] == s1[j]:
-            dp[j] = tmp+1
+            dp[i][j] = dp[i-1][j-1] + 1
         else:
-            tmp = max(tmp, dp[j])
-    print(dp)
-print(max(dp))
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j-1])
+[print(*dp[i]) for i in range(len(s2))]
+
+print(dp[-1][-1])
+
+# dp = [0] * 1000
+# for i in range(len(s2)):
+#     cnt = 0
+#     for j in range(len(s1)):
+#         if s2[i] == s1[j]:
+#             dp[j] = max(cnt +1, dp[j]+1)
+#         else:
+#             cnt = max(cnt, dp[j])
+# print(max(dp))
