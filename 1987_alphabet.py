0b7683c20c8dc25fad93aeb4128fd4fd9681e4ea
L1m3Kun <sungku2757@gmail.com>
2023-05-12 17:58:07 +0900

    save

--- 1987_alphabet.py
+++ 1987_alphabet.py
@@ -1,24 +1,29 @@
 # 1987 알파벳
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 R, C = map(int, input().split())
 board = [input().strip() for _ in range(R)]
 
-que = [(0,0, board[0][0])]
+que = deque([(0,0, board[0][0])])
 # visited = board[0][0]
 maxV = 0
-max_len = set()
-dir = [(0,-1), (0,1), (1,0), (-1,0)]
+# max_len = set()
+dir = [(0, 1), (0,-1), (1,0), (-1,0)]
 while que:
-    i, j, visited = que.pop()
-    # maxV = max(maxV, len(visited))
-    
+    i, j, visited = que.popleft()
+    if maxV == 26 or maxV == R*C:
+        break
+    if maxV > len(visited):
+        continue
     # print(visited)
     for di, dj in dir:
         ni, nj = i+di, j+dj
         if 0<= ni <R and 0<=nj<C and board[ni][nj] not in visited:
             que.append((ni, nj, visited+board[ni][nj]))
-            max_len.add(len(visited)+1)
-print(max(max_len))
+            # max_len.add(len(visited)+1)
+            maxV = max(maxV, len(visited)+1)
+# print(max(max_len))
+print(maxV)
 
