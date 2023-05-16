4c3328dbcd49d53fae8e86bdf4b3dbceb87101f9
L1m3Kun <sungku2757@gmail.com>
2023-05-16 09:24:39 +0900

    save

--- Others/거리두기_확인하기.py
+++ Others/거리두기_확인하기.py
@@ -1,28 +1,33 @@
 def check(y, x, arr):
-    for i in range(-2,3):
+    for i in range(3):
         for j in range(abs(i)-2, 3-abs(i)):
             if 0<=y + i < 5 and 0<= x+j < 5 and  arr[y+i][x+j] == 'P':
                 if abs(i) + abs(j) == 1:
                     print(y,x, 1)
                     return 1
-                elif i == 0 and j != 0 and arr[y][x+j//2] != 'X':
+                if i == 0 and j != 0 and arr[y][x+j//2] != 'X':
                     print(y,x, 2)
                     return 1
-                elif i != 0 and j == 0 and arr[y+i//2][x] != 'X':
+                if i != 0 and j == 0 and arr[y+i//2][x] != 'X':
                     print(y,x, 3)
                     return 1
-                elif i != 0 and j != 0:
-                    if y+1<5 and arr[y+i//2][x] != 'X':
+                
+                if i != 0 and j != 0:
+                    if arr[y+1][x] != 'X':
+                        print(y, x, 6)
+                        return 1
+                    else:
                         if j<0 and arr[y][x-1] != 'X':
                             print(y, x, 4)
                             return 1
-                        if j>0 and arr[y][x+1] != 'X':
+                        elif j>0 and arr[y][x+1] != 'X':
                             print(y, x, 5)
                             return 1
 
 def solution(places):
     answer = []
     for room in range(5):
+        print('page', room+1)
         i= j = 0
         while i< 5 and j <5:
             if places[room][i][j] == 'P':
@@ -36,12 +41,9 @@ def solution(places):
                 j = 0
         else:
             answer.append(1)
+        print()
     return answer
-arr = [
-    ['XXXXX','XXXXX','XXXXX','XXXXX','XXXXX'],
-    ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
-    ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], 
-    ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
-    ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]
-]
-print(solution(arr))
+
+places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+
+print(solution(places))
