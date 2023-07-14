2f2b1bc174bd271d73209e006306252ca0ff381e
강성구 <sungku2757@gmail.com>
2023-07-14 17:23:21 +0900

    save

--- 1976_letsTravel.py
+++ 1976_letsTravel.py
@@ -3,34 +3,36 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+# Input
 N = int(input())
 M = int(input())
-
 linked = [ [] for _ in range(N+1)]
-# print(linked)
-for country in range(1,N+1):
+for country in range(1, N+1):
     route = list(map(int, input().split()))
     for b in range(N):
         if route[b]:
             linked[country].append(b+1)
-travel = deque(map(int,input().split()))
-# print(linked)
+plan = list(map(int, input().split()))
 
-check = set()
-# for i in linked:
-#     for j in range(M):
-#         if travel[j] in linked[i]:
-#             check.add(travel[j])
-# print(check)
-check.add(travel[0])
-for spot in travel:
-    for i in range(M):
-        if travel[i] in linked[spot]:
-            check.add(travel[i])
-# print(check)
-print("YES") if len(check) == M else print("NO")
+que = deque([plan[0]])
+passed = set([plan[0]])
+while que:
+    spot = que.popleft()
+    for i in range(1, N+1):
+        for element in linked[spot]:
+            if element in plan and element not in passed:
+                que.append(element)
+                passed.add(element)
+for i in set(plan):
+    if i not in passed:
+        print("NO")
+        break
+else:
+    print("YES")
 
-# 1976 여행가자
+
+
+'''
 import sys
 input = sys.stdin.readline
 
@@ -38,18 +40,26 @@ N = int(input())
 M = int(input())
 
 linked = [ [] for _ in range(N+1)]
-# print(linked)
+
 for country in range(1,N+1):
     route = list(map(int, input().split()))
     for b in range(N):
         if route[b]:
             linked[country].append(b+1)
+
 travel = list(map(int,input().split()))
 
-check = set()
+check = set([travel[0]])
 for i in range(1,N+1):
     for j in range(M):
         if travel[j] in linked[i]:
-            check.add(j)
+            check.add(travel[j])
 
-print("YES") if len(check) == M else print("NO")
+for element in set(travel):
+    if element not in check:
+        print("NO")
+        break
+else:
+    print("YES")
+
+'''
