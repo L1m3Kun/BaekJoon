801f665c60a17a5d55cb5d9d789723a4fa7af943
강성구 <sungku2757@gmail.com>
2023-09-07 18:14:05 +0900

    6주차 중간

--- 12919_A_n_B_2.py
+++ 12919_A_n_B_2.py
@@ -1,4 +1,3 @@
-# 12919 A와 B 2
 import sys
 
 input = sys.stdin.readline
@@ -7,23 +6,129 @@ S = input().strip()
 T = input().strip()
 
 
+def dfs(t):
+    stack = [t]
+    while stack:
+        s = stack.pop()
+        if len(s) == len(S):
+            if s == S:
+                return 1
+            continue
+        if s[-1] == "A":
+            stack.append(s[:-1])
+        if s[0] == "B":
+            stack.append(s[::-1][:-1])
+    return 0
+
+
+print(dfs(T))
+
+
+"""
+# # 12919 A와 B 2
+# import sys
+# from collections import deque
+
+# input = sys.stdin.readline
+
+# S = input().strip()
+# T = input().strip()
+
+
+# def addA(s):
+#     return s + "A"
+
+
+# def addB(s):
+#     return (s + "B")[::-1]
+
+
+# def bfs(s):
+#     que = deque([s])
+#     # visited = set([s, a, b])
+#     while que:
+#         tmp = que.popleft()
+#         if len(tmp) == len(T):
+#             if tmp == T:
+#                 return 1
+#             continue
+
+#         que.append(addA(tmp))
+#         que.append(addB(tmp))
+#         # a = addA(tmp)
+#         # b = addB(tmp)
+#         # if a not in visited:
+#         #     visited.add(a)
+#         # que.append(a)
+#         # if b not in visited:
+#         #     visited.add(b)
+#         # que.append(b)
+#     return 0
+
+
+# # def dfs(s):
+# #     stack = [s]
+# #     visited = set()
+# #     while stack:
+# #         tmp = stack.pop()
+# #         if len(tmp) == len(T):
+# #             if tmp == T:
+# #                 return 1
+# #             continue
+# #         if tmp in visited:
+# #             continue
+# #         else:
+# #             visited.add(tmp)
+# #             stack.append(addA(tmp))
+# #             stack.append(addB(tmp))
+
+# #         # a = addA(tmp)
+# #         # b = addB(tmp)
+# #         # if a not in visited:
+# #         #     stack.append(a)
+# #         #     visited.add(a)
+# #         # if b not in visited:
+# #         #     stack.append(b)
+# #         #     visited.add(b)
+# #     return 0
+
+
+# print(dfs(S))
+# # print(bfs(S))
+# # print(answer)
+# 12919 A와 B 2
+import sys
+
+input = sys.stdin.readline
+
+S = input().strip()
+T = input().strip()
+visited = set()
+
+
 def addA(s):
-    s += "A"
+    return s + "A"
 
 
 def addB(s):
-    s += "B"
-    tmp = ""
-    for s in range(len(s) - 1, -1, -1):
-        tmp += s
-    s = tmp
+    return (s + "B")[::-1]
 
 
-def bfs(s):
+def dfs(s):
     if len(s) == len(T):
         if s == T:
-            return 1
+            print(1)
+            exit(0)
         return
-    addA(s)
+    if s in visited:
+        return
+    else:
+        visited.add(s)
+        dfs(addA(s))
+        dfs(addB(s))
+    return
 
-    addB(s)
+
+dfs(S)
+
+"""
--- 1943_devide_coin.py
+++ 1943_devide_coin.py
@@ -2,48 +2,62 @@
 import sys
 
 input = sys.stdin.readline
+dp = [0] * 50000
 
-
-# 동전으로 총 금액의 반액 만들기
-def make_money_with_coin(coinList: list, coins: dict):
-    money = 0
-
-    return 0
-
-
-# test case
 for _ in range(3):
-    # define
+    dp.clear()
     coins = {}
     total = 0
-
-    # input
     N = int(input())
-    flag = 0
     for i in range(N):
         coin, cnt = map(int, input().split())
         coins[coin] = cnt
         total += coin * cnt
-        if cnt % 2:
-            flag += 0
-        else:
-            flag += 1
-    if flag == N:
-        print(1)
-    elif total % 2:
+    if total % 2:
         print(0)
     else:
-        total /= 2
+        total //= 2
+        print(dp[total])
+# # 동전으로 총 금액의 반액 만들기
+# def make_money_with_coin(coinList: list, coins: dict):
+#     money = 0
 
-        coin_list = list(coins.keys())
-        coin_list.sort(reverse=1)
-        print(make_money_with_coin(coin_list, coins))
+#     return 0
 
-"""
-예외 케이스
-4
-3 7
-5 2
-7 3
-8 1
-"""
+
+# # test case
+# for _ in range(3):
+#     # define
+#     coins = {}
+#     total = 0
+
+#     # input
+#     N = int(input())
+#     flag = 0
+#     for i in range(N):
+#         coin, cnt = map(int, input().split())
+#         coins[coin] = cnt
+#         total += coin * cnt
+#         if cnt % 2:
+#             flag += 0
+#         else:
+#             flag += 1
+#     if flag == N:
+#         print(1)
+#     elif total % 2:
+#         print(0)
+#     else:
+#         total /= 2
+
+#         coin_list = list(coins.keys())
+#         coin_list.sort(reverse=1)
+#         print(make_money_with_coin(coin_list, coins))
+
+# """
+# 예외 케이스
+# 4
+# 3 7
+# 5 2
+# 7 3
+# 8 1
+# """
