aa410020222e9079482ffb29b81d9abb715b60dc
L1m3Kun <sungku2757@gmail.com>
2024-01-12 21:29:21 +0900

    15684 사다리 조작

--- Back_tracking/15684_fake_ladder.py
+++ Back_tracking/15684_fake_ladder.py
@@ -1,25 +1,53 @@
+# 15684 사다리 조작
 import sys
 input = sys.stdin.readline
 
-def backtracking(i, j, H, start):
-    if i == H:
-        return
-    if not 0<= j < N:
-        return
-    if ladder[i][j]:
-        if j < N:
-            backtracking(i, j+1,H, start)
+
+def check_ladder() -> bool:
+    for i in range(N):
+        y, x = 0, i
+        while y < H:
+            if ladders[y][x] == 1:
+                x -= 1
+            elif ladders[y][x] == 2:
+                x += 1
+            y += 1
+        if x != i:
+            return 0
+    return 1
+
+
+def bt(cnt:int, start_i:int) -> None:
+    global ans
     
+    if check_ladder():
+        ans = min(cnt, ans)
+        return
+    if cnt == 3:
+        return 
+    if cnt >= ans:
+        return
+    for i in range(start_i, H):
+        for j in range(N-1):
+            if not ladders[i][j] and not ladders[i][j+1]:
+                ladders[i][j] = 2
+                ladders[i][j+1] = 1
+                bt(cnt+1, i)
+                ladders[i][j] = 0
+                ladders[i][j+1] = 0
+    return
 
 
-N, M, H = map(int, input().split())
-if not M:
-    print(0)
-elif M == H*(N-1)/2:
-    print(-1)
-else:
-    ladder = [[0] * N for _ in range(H)]
+if __name__ == "__main__":
+    N, M, H = map(int, input().split())
+    ladders = [[0] * N for _ in range(H)]
+    visited = [[0] * N for _ in range(H)]
     for _ in range(M):
-        x, y = map(int, input().split())
-        ladder[y-1][x-1] = 1
-    [print(*ladder[_]) for _ in range(H)]
+        # 1 -> 왼쪽, 2 -> 오른쪽
+        a, b = map(int, input().split())
+        ladders[a-1][b-1] = 2
+        ladders[a-1][b] = 1
+    ans = 5
+    bt(0,0)
+    print(-1 if ans > 3 else ans)
+        
