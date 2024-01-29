41cb1485fee0bc50744e4240bdab853a381b5cd0
L1m3Kun <sungku2757@gmail.com>
2024-01-29 18:02:55 +0900

    3015 오아시스 재결합

--- 3015_regroup_oasis.py
+++ 3015_regroup_oasis.py
@@ -9,22 +9,27 @@ def solution():
     for _ in range(int(input())):
         fan = int(input())
         
-        if len(stack):
-            if fan > stack[-1][0]:
-                while fan < stack[-1][0]:
-                    cnt += stack.pop()[1]
-                
-                
-                
-            elif fan == stack[-1][0]:
+        while stack and fan > stack[-1][0]:
+            cnt += stack.pop()[1]
+        
+        if stack:
+            if fan == stack[-1][0]:
                 cnt += stack[-1][1]
+                if len(stack) > 1:
+                    cnt += 1
                 stack[-1][1] += 1
             else:
                 stack.append([fan, 1])
-                
+                cnt += 1
         else:
             stack.append([fan, 1])
+
+
+         
+         
     print(cnt)
 
 if __name__ == "__main__":
     solution()
+
+    
