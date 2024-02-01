8eadc4a5ab7408a7eb36e4bfc8a39e873aa8a089
L1m3Kun <sungku2757@gmail.com>
2024-02-02 00:42:37 +0900

    24313 알고리즘 수업 - 점근적 표기 1

--- Normal_Math/24313_.py
+++ Normal_Math/24313_.py
@@ -7,7 +7,7 @@ def solution():
     a1, a0 = map(int, input().split())
     c = int(input())
     n0 = int(input())
-    if n0 * c >= a1*n0 + a0:
+    if n0 * c >= a1*n0 + a0 and a1 <= c:
         print(1)
     else:
         print(0)
